567cd687e59f2cda1868922e280a4007f7a90622
Juliana Davila <judaro13@gmail.com>
2013-09-03 19:44:06 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MokaProyect::Application.config.secret_key_base = 'e37f527509f9a0b48f8aa708bc47d30036dbe6b34407f65e1564b2c51156479997d1d97be931bbddfb585813a65e4c44f29f0da2735de9ac035bc192cd1acfad'
